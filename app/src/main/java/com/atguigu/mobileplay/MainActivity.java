f320f8f104bf2ab80757ac2ee11a917c1d245025
z920393030 <9203930303@qq.com>
2017-05-19 15:03:23 +0800

    解决横竖屏切换导致的Fragment内容重叠问题

--- app/src/main/java/com/atguigu/mobileplay/MainActivity.java
+++ app/src/main/java/com/atguigu/mobileplay/MainActivity.java
@@ -1,13 +1,121 @@
 package com.atguigu.mobileplay;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.RadioGroup;
+
+import com.atguigu.mobileplay.fragment.BaseFragment;
+import com.atguigu.mobileplay.pager.LocalAudioPager;
+import com.atguigu.mobileplay.pager.LocalVideoPager;
+import com.atguigu.mobileplay.pager.NetAudioPager;
+import com.atguigu.mobileplay.pager.NetVideoPager;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RadioGroup rg_main;
+    private ArrayList<BaseFragment> fragments;
+    private int position;
 
+    private Fragment tempFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rg_main = (RadioGroup)findViewById(R.id.rg_main);
+        initFragment();
+        rg_main.setOnCheckedChangeListener(new MyOnCheckedChangeListener());
+        rg_main.check(R.id.rb_local_video);
+    }
+
+    private void initFragment() {
+        fragments = new ArrayList<>();
+        fragments.add(new LocalVideoPager());
+        fragments.add(new LocalAudioPager());
+        fragments.add(new NetAudioPager());
+        fragments.add(new NetVideoPager());
+    }
+
+    private class MyOnCheckedChangeListener implements RadioGroup.OnCheckedChangeListener {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            switch (checkedId){
+                case R.id.rb_local_video:
+                    position = 0;
+                    break;
+                case R.id.rb_local_audio:
+                    position = 1;
+                    break;
+
+                case R.id.rb_net_audio:
+                    position = 2;
+                    break;
+
+                case R.id.rb_net_video:
+                    position  = 3;
+                    break;
+            }
+            BaseFragment currentFragment = fragments.get(position);//要显示的Fragment
+            addFragment(currentFragment);
+        }
+    }
+
+    private void addFragment(BaseFragment currentFragment) {
+        if(tempFragment != currentFragment){
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            if(!currentFragment.isAdded()){
+                if(tempFragment != null){
+                    ft.hide(tempFragment);
+                }
+                ft.add(R.id.fl_content,currentFragment);
+            }else{
+                if(tempFragment != null){
+                    ft.hide(tempFragment);
+                }
+                ft.show(currentFragment);
+            }
+
+            ft.commit();
+            tempFragment = currentFragment;
+
+        }
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("MainActivity","onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("MainActivity","onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("MainActivity","onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("MainActivity","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("MainActivity","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("MainActivity","onDestroy");
     }
 }
