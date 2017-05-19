30a7727f24b900219753be84be3e8c9f2f75b917
z920393030 <9203930303@qq.com>
2017-05-19 16:34:36 +0800

    视频列表的Item完成和相关数据处理

--- app/src/main/java/com/atguigu/mobileplay/MainActivity.java
+++ app/src/main/java/com/atguigu/mobileplay/MainActivity.java
@@ -1,5 +1,9 @@
 package com.atguigu.mobileplay;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -83,6 +87,28 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
+    /**
+     * 解决安卓6.0以上版本不能读取外部存储权限的问题
+     * @param activity
+     * @return
+     */
+    public static boolean isGrantExternalRW(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && activity.checkSelfPermission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            activity.requestPermissions(new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 1);
+
+            return false;
+        }
+
+        return true;
+    }
+
+
     @Override
     protected void onRestart() {
         super.onRestart();
--- app/src/main/java/com/atguigu/mobileplay/pager/LocalVideoPager.java
+++ app/src/main/java/com/atguigu/mobileplay/pager/LocalVideoPager.java
@@ -1,11 +1,23 @@
 package com.atguigu.mobileplay.pager;
 
-import android.graphics.Color;
-import android.view.Gravity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.atguigu.mobileplay.R;
+import com.atguigu.mobileplay.adapter.LocalVideoAdapter;
+import com.atguigu.mobileplay.domain.MediaItem;
 import com.atguigu.mobileplay.fragment.BaseFragment;
+
+import java.util.ArrayList;
 
 /**
  * Created by My on 2017/5/19.
@@ -13,19 +25,74 @@ import com.atguigu.mobileplay.fragment.BaseFragment;
 
 public class LocalVideoPager extends BaseFragment {
 
-    private TextView textView;
+    private ListView lv;
+    private TextView tv_nodata;
+    private ArrayList<MediaItem> mediaItems;
+    private LocalVideoAdapter adapter;
     @Override
     public View initView() {
-        textView = new TextView(context);
-        textView.setTextSize(30);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View  view = View.inflate(context, R.layout.fragment_local_video_pager,null);
+        lv = (ListView) view.findViewById(R.id.lv);
+        tv_nodata = (TextView) view.findViewById(R.id.tv_nodata);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                MediaItem item = adapter.getItem(position);
+                Toast.makeText(context, ""+item.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
     }
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(mediaItems!=null && mediaItems.size() >0) {
+                tv_nodata.setVisibility(View.GONE);
+
+                adapter = new LocalVideoAdapter(context,mediaItems);
+                lv.setAdapter(adapter);
+
+            }else {
+                tv_nodata.setVisibility(View.VISIBLE);
+            }
+        }
+    };
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText("本地视频的内容");
+        getData();
+    }
+
+    private void getData() {
+        new Thread(){
+            public void run(){
+                mediaItems = new ArrayList<MediaItem>();
+                ContentResolver resolver = context.getContentResolver();
+                Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                String[] objs = {
+                        MediaStore.Video.Media.DISPLAY_NAME,//视频在sdcard上的名称
+                        MediaStore.Video.Media.DURATION,//视频时长
+                        MediaStore.Video.Media.SIZE,//视频文件的大小
+                        MediaStore.Video.Media.DATA//视频播放地址
+                };
+                Cursor cursor = resolver.query(uri, objs, null, null, null);
+
+                if(cursor != null) {
+                    while (cursor.moveToNext()){
+                        String name = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME));
+                        long duration = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.DURATION));
+                        long size = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.SIZE));
+                        String data = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
+
+                        mediaItems.add(new MediaItem(name,duration,size,data));
+                        handler.sendEmptyMessage(0);
+                    }
+                    cursor.close();
+                }
+            }
+        }.start();
     }
 }
